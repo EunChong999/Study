176e4c9b03a747fe2d23fd9c84b11c335a686d02
김은총 <136239807+EunChong999@users.noreply.github.com>
2024-05-27 15:40:13 +0900

    feat : 서버 개발중

---
+++ Assets/Server/PhotonChatManager.cs
@@ -0,0 +1,144 @@
+using ExitGames.Client.Photon;
+using Photon.Chat;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhotonChatManager : MonoBehaviour, IChatClientListener
+{
+    #region Setup
+    [SerializeField] GameObject joinChatButton;
+    ChatClient chatClient;
+    bool isConnected;
+    [SerializeField] string username;
+    public void UsernameOnValueChange(string valueIn)
+    {
+        username = valueIn;
+    }
+    public void ChatConnectOnClick()
+    {
+        isConnected = true;
+        chatClient = new ChatClient(this);
+        //chatClient.ChatRegion = "US";
+        chatClient.Connect(PhotonNetwork.PhotonServerSettings.ChatAppID, PhotonNetwork.versionPUN, new Photon.Chat.AuthenticationValues(username));
+        Debug.Log("Connenting");
+    }
+    #endregion Setup
+    #region General
+    [SerializeField] GameObject chatPanel;
+    [SerializeField] GameObject lobyPanel;
+    string privateReceiver = "";
+    string currentChat;
+    [SerializeField] TMP_InputField chatField;
+    [SerializeField] TextMeshProUGUI chatDisplay;
+
+    void Update()
+    {
+        if (isConnected)
+        {
+            chatClient.Service();
+        }
+        if (chatField.text != "" && Input.GetKey(KeyCode.Return))
+        {
+            SubmitPublicChatOnClick();
+            SubmitPrivateChatOnClick();
+        }
+    }
+    #endregion General
+    #region PublicChat
+    public void SubmitPublicChatOnClick()
+    {
+        if (privateReceiver == "")
+        {
+            chatClient.PublishMessage("RegionChannel", currentChat);
+            chatField.text = "";
+            currentChat = "";
+        }
+    }
+    public void TypeChatOnValueChange(string valueIn)
+    {
+        currentChat = valueIn;
+    }
+    #endregion PublicChat
+    #region PrivateChat
+    public void ReceiverOnValueChange(string valueIn)
+    {
+        privateReceiver = valueIn;
+    }
+    public void SubmitPrivateChatOnClick()
+    {
+        if (privateReceiver != "")
+        {
+            chatClient.SendPrivateMessage(privateReceiver, currentChat);
+            chatField.text = "";
+            currentChat = "";
+        }
+    }
+    #endregion PrivateChat
+    #region Callbacks
+    public void DebugReturn(DebugLevel level, string message)
+    {
+        //throw new System.NotImplementedException();
+    }
+    public void OnChatStateChange(ChatState state)
+    {
+        if (state == ChatState.Uninitialized)
+        {
+            isConnected = false;
+            joinChatButton.SetActive(true);
+            chatPanel.SetActive(false);
+        }
+    }
+    public void OnConnected()
+    {
+        Debug.Log("Connected");
+        joinChatButton.SetActive(false);
+        chatClient.Subscribe(new string[] { "RegionChannel" });
+    }
+    public void OnDisconnected()
+    {
+        isConnected = false;
+        joinChatButton.SetActive(true);
+        chatPanel.SetActive(false);
+    }
+    public void OnGetMessages(string channelName, string[] senders, object[] messages)
+    {
+        string msgs = "";
+        for (int i = 0; i < senders.Length; i++)
+        {
+            msgs = string.Format("{0}: {1}", senders[i], messages[i]);
+            chatDisplay.text += "\n" + msgs;
+            Debug.Log(msgs);
+        }
+    }
+    public void OnPrivateMessage(string sender, object message, string channelName)
+    {
+        string msgs = "";
+        msgs = string.Format("(Private) {0}: {1}", sender, message);
+        chatDisplay.text += "\n " + msgs;
+        Debug.Log(msgs);
+
+    }
+    public void OnStatusUpdate(string user, int status, bool gotMessage, object message)
+    {
+        throw new System.NotImplementedException();
+    }
+    public void OnSubscribed(string[] channels, bool[] results)
+    {
+        lobyPanel.SetActive(false);
+        chatPanel.SetActive(true);
+    }
+    public void OnUnsubscribed(string[] channels)
+    {
+        throw new System.NotImplementedException();
+    }
+    public void OnUserSubscribed(string channel, string user)
+    {
+        throw new System.NotImplementedException();
+    }
+    public void OnUserUnsubscribed(string channel, string user)
+    {
+        throw new System.NotImplementedException();
+    }
+    #endregion Callbacks
+}
